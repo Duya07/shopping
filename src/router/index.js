910e4e484c751fd4921d0b0fcf4623a38fd004b4
duya <du77777@foxmail.com>
2020-09-18 12:44:52 +0800

    router-link标签能用了 但是现在有个bug 会显示两次首页标签

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
+import Vue from 'vue'
 import VueRouter from 'vue-router'
 import login from '../components/login.vue'
 import home from '../components/home.vue'
+
+Vue.use(VueRouter)
 
 //安装路由
 const routes = [
