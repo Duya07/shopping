41c6f5d9336841a814c9b93879fc017dc0066a86
duya <du77777@foxmail.com>
2020-09-18 14:41:12 +0800

    有时候两次首页标签不会显示两次， 但是会提示没有JavaScript

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,7 +4,6 @@
         <router-link to="/login">登录</router-link>
         <router-view>
         </router-view>
-
     </div>
 </template>
 
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,39 +1,37 @@
 <template>
-	<div id="login">
-		<form>
-			用户名：<input type="text" id="name"><br>
-			密码：<input type="password" id="password"><br>
-			<button type="button" id="loginin">登录</button>
-			<button type="button">注册</button>
-		</form>
-	</div>
+    <div id="login">
+        <form>
+            用户名：<input type="text" id="name"><br>
+            密码：<input type="password" id="password"><br>
+            <button type="button" id="loginButton">登录</button>
+            <button type="button">注册</button>
+        </form>
+    </div>
 </template>
 
 <script>
-	import $ from 'jquery'
-	$(document).ready(function () {
-		$('#login').click(function(){
-			console.log("aaa");
-			// $.ajax({
-			// 	type: "GET",
-			// 	url: "test.json",
-			// 	data: {username:$("#username").val(), content:$("#content").val()},
-			// 	dataType: "json",
-			// 	success: function(data){
-			// 		$('#resText').empty();   //清空resText里面的所有内容
-			// 		var html = '';
-			// 		$.each(data, function(commentIndex, comment){
-			// 			html += '<div class="comment"><h6>' + comment['username']
-			// 					+ ':</h6><p class="para"' + comment['content']
-			// 					+ '</p></div>';
-			// 		});
-			// 		$('#resText').html(html);
-			// 	}
-			// });
-		});
-	})
-	export default {
-		name: 'login',
-	}
+    import $ from 'jquery'
+    $('#loginButton').click(function () {
+        console.log("aaa");
+        // $.ajax({
+        // 	type: "GET",
+        // 	url: "test.json",
+        // 	data: {username:$("#username").val(), content:$("#content").val()},
+        // 	dataType: "json",
+        // 	success: function(data){
+        // 		$('#resText').empty();   //清空resText里面的所有内容
+        // 		var html = '';
+        // 		$.each(data, function(commentIndex, comment){
+        // 			html += '<div class="comment"><h6>' + comment['username']
+        // 					+ ':</h6><p class="para"' + comment['content']
+        // 					+ '</p></div>';
+        // 		});
+        // 		$('#resText').html(html);
+        // 	}
+        // });
+    })
+    export default {
+        name: 'login',
+    }
 
 </script>
