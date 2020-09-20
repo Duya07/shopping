0add3bdfb0189f6cb68a633cdc6e5fb81ed151cd
duya <du77777@foxmail.com>
2020-09-20 18:53:54 +0800

    今天我把console打印报错解决了 现在可以安心完成项目了

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,35 +3,20 @@
         <form>
             用户名：<input type="text" id="name"><br>
             密码：<input type="password" id="password"><br>
-            <button type="button" id="loginButton">登录</button>
+            <button @click="btnclick" type="button" id="loginButton">登录</button>
             <button type="button">注册</button>
         </form>
     </div>
 </template>
 
 <script>
-    import $ from 'jquery'
-    $('#loginButton').click(function () {
-        console.log("aaa");
-        // $.ajax({
-        // 	type: "GET",
-        // 	url: "test.json",
-        // 	data: {username:$("#username").val(), content:$("#content").val()},
-        // 	dataType: "json",
-        // 	success: function(data){
-        // 		$('#resText').empty();   //清空resText里面的所有内容
-        // 		var html = '';
-        // 		$.each(data, function(commentIndex, comment){
-        // 			html += '<div class="comment"><h6>' + comment['username']
-        // 					+ ':</h6><p class="para"' + comment['content']
-        // 					+ '</p></div>';
-        // 		});
-        // 		$('#resText').html(html);
-        // 	}
-        // });
-    })
     export default {
         name: 'login',
+        methods:{
+            btnclick(){
+                console.log('sssssssss')
+            }
+        }
     }
 
 </script>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from "@/router";
 
 Vue.config.productionTip = false
 
-Vue.use(router)
-
 new Vue({
   render: h => h(App),
   router
