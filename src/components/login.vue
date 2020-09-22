4010361c668cafe66e3f56f0c8671471114a30ec
duya <du77777@foxmail.com>
2020-09-22 09:50:46 +0800

    重写了axios 可以对后端接口进行请求了

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="login">
         <form>
-            用户名：<input type="text" id="name"><br>
-            密码：<input type="password" id="password"><br>
+            用户名：<input type="text" id="name" v-model="username" placeholder="请输入账号或邮箱"><br>
+            密码：<input type="password" id="password" v-model="password" placeholder="请输入密码"><br>
             <button @click="btnclick" type="button" id="loginButton">登录</button>
             <button type="button">注册</button>
         </form>
@@ -12,9 +12,23 @@
 <script>
     export default {
         name: 'login',
-        methods:{
-            btnclick(){
-                console.log('sssssssss')
+        data: function(){
+            return{
+                username: "",
+                password: ""
+            }
+        },
+        methods: {
+            btnclick() {
+                this.$axios.get('/api/hello', {
+                    params: {
+                        username: this.username,
+                        password: this.password
+                    }
+                }).then((response) => {
+                    var data = response.data;
+                    console.log(data);
+                })
             }
         }
     }
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    configureWebpack: {
+        devServer: {
+            port: 8081,
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:8080/',
+                    changeOrigin: true, //是否跨域
+                    pathRewrite: {
+                        '^/api': '' //规定请求地址以什么作为开头
+                    }
+                }
+            }
+        }
+    }
+}
