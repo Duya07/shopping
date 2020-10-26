a0b9ceda04c1d57d0c748843363c937f08288e78
duya <du77777@foxmail.com>
2020-10-26 23:56:24 +0800

    8080 端口调试一切正常 很符合预期 8081 是问题的地方 明天继续

--- src/main/java/com/duya/shopping/config/SecurityConfig.java
+++ src/main/java/com/duya/shopping/config/SecurityConfig.java
@@ -1,20 +1,59 @@
-//package com.duya.shopping.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.formLogin()
-//                .loginProcessingUrl("/login")
-//                .and().cors();
-//    }
-//}
+package com.duya.shopping.config;
+
+import com.alibaba.fastjson.JSON;
+import com.duya.shopping.utils.ServiceResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.PrintWriter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/hello").hasRole("admin")
+                .anyRequest().permitAll()
+
+                .and()
+                .formLogin().loginProcessingUrl("/login").permitAll()
+                .successHandler((req, resp, authentication) -> {
+                    Object principal = authentication.getPrincipal();
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(JSON.toJSONString(ServiceResult.success(principal)));
+                    out.flush();
+                    out.close();
+                })
+                .failureHandler((req, resp, authentication) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(JSON.toJSONString(ServiceResult.error()));
+                    out.flush();
+                    out.close();
+                });
+
+        // 退出还没好
+        http.logout();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin").roles("admin");
+    }
+}
--- src/main/java/com/duya/shopping/controller/test.java
+++ src/main/java/com/duya/shopping/controller/test.java
@@ -5,6 +5,7 @@ import com.duya.shopping.service.UserImpl;
 import com.duya.shopping.utils.ServiceResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,13 +14,18 @@ public class test {
     @Autowired
     private UserImpl user;
 
-    @GetMapping("login")
-    public String login() {
-        ServiceResult sr = ServiceResult.success("login接口成功了兄弟");
-        return JSON.toJSONString(sr);
+//    @PostMapping("/login")
+//    public String login() {
+//        ServiceResult sr = ServiceResult.success("login接口成功了兄弟");
+//        return JSON.toJSONString(sr);
+//    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
     }
 
-    @GetMapping("registered")
+    @GetMapping("/registered")
     public String registered(String username, String password, String email, String mobile) {
         user.registered(username, password, email, mobile);
         return "ok";
