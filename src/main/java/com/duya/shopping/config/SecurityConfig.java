d2628358bf1ddfc1226926de7b66c9b21e25e5bf
duya <du77777@foxmail.com>
2020-10-27 20:15:28 +0800

    后端测试没有问题了 前端向login post请求 json 格式  {"username": "admin", "password": "admin"} 这样子就能正确回应 现在就需要对前端json格式设置了

--- src/main/java/com/duya/shopping/config/SecurityConfig.java
+++ src/main/java/com/duya/shopping/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.duya.shopping.config;
 
 import com.alibaba.fastjson.JSON;
+import com.duya.shopping.filter.CustomAuthenticationFilter;
 import com.duya.shopping.utils.CodeMessage;
 import com.duya.shopping.utils.ServiceResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,9 +11,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 @Configuration
@@ -26,32 +36,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/hello").hasRole("admin")
+                .antMatchers("/manage").hasRole("admin")
                 .anyRequest().permitAll()
 
                 .and()
                 .formLogin().loginProcessingUrl("/login")
-                .successHandler((req, resp, authentication) -> {
-                    Object principal = authentication.getPrincipal();
-                    resp.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = resp.getWriter();
-                    out.write(JSON.toJSONString(ServiceResult.success(principal)));
-                    out.flush();
-                    out.close();
-                })
-                .failureHandler((req, resp, authentication) -> {
-                    resp.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = resp.getWriter();
-                    out.write(JSON.toJSONString(ServiceResult.error(
-                            new ServiceResult.DefaultMessage<>("003", "账号或者密码错误"))));
-                    out.flush();
-                    out.close();
-                }).permitAll()
+//                .successHandler((req, resp, authentication) -> {
+//                    Object principal = authentication.getPrincipal();
+//                    resp.setContentType("application/json;charset=utf-8");
+//                    PrintWriter out = resp.getWriter();
+//                    out.write(JSON.toJSONString(ServiceResult.success(principal)));
+//                    out.flush();
+//                    out.close();
+//                })
+//                .failureHandler((req, resp, exception) -> {
+//                    System.out.println(exception);
+//                    resp.setContentType("application/json;charset=utf-8");
+//                    PrintWriter out = resp.getWriter();
+//                    out.write(JSON.toJSONString(ServiceResult.error(
+//                            new ServiceResult.DefaultMessage<>("003", "账号或者密码错误"))));
+//                    out.flush();
+//                    out.close();
+//                })
+                .permitAll()
                 .and()
                 .logout().permitAll()
                 .and()
                 .csrf().disable();
 
+        http.addFilter(customAuthenticationFilter());
 
     }
 
@@ -60,5 +73,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password("admin").roles("admin");
+    }
+
+
+    /*
+    json 格式  {"username": "admin", "password": "admin"}
+     */
+    @Bean
+    CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
+        CustomAuthenticationFilter filter = new CustomAuthenticationFilter();
+        filter.setAuthenticationSuccessHandler(new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest req, HttpServletResponse resp, Authentication auth) throws IOException, ServletException {
+                Object principal = auth.getPrincipal();
+                resp.setContentType("application/json;charset=utf-8");
+                PrintWriter out = resp.getWriter();
+                out.write(JSON.toJSONString(ServiceResult.success(principal)));
+                out.flush();
+                out.close();
+            }
+        });
+        filter.setAuthenticationFailureHandler(new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest req, HttpServletResponse resp, AuthenticationException e) throws IOException, ServletException {
+                resp.setContentType("application/json;charset=utf-8");
+                PrintWriter out = resp.getWriter();
+                out.write(JSON.toJSONString(ServiceResult.error(
+                        new ServiceResult.DefaultMessage<>("003", "账号或者密码错误")
+                )));
+                out.flush();
+                out.close();
+            }
+
+        });
+        filter.setAuthenticationManager(authenticationManagerBean());
+        return filter;
     }
 }
--- src/main/java/com/duya/shopping/controller/test.java
+++ src/main/java/com/duya/shopping/controller/test.java
@@ -14,17 +14,6 @@ public class test {
     @Autowired
     private UserImpl user;
 
-//    @PostMapping("/login")
-//    public String login() {
-//        ServiceResult sr = ServiceResult.success("login接口成功了兄弟");
-//        return JSON.toJSONString(sr);
-//    }
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
-
     @GetMapping("/registered")
     public String registered(String username, String password, String email, String mobile) {
         user.registered(username, password, email, mobile);
