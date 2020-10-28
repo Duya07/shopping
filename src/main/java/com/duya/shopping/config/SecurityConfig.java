61b0b7447e72086d5389b329371de69624b2f414
duya <du77777@foxmail.com>
2020-10-28 15:06:05 +0800

    现在对前端进行 权限调试 后端没问题

--- src/main/java/com/duya/shopping/config/SecurityConfig.java
+++ src/main/java/com/duya/shopping/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -33,19 +34,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    /*
+    /manage/** 需要admin权限 其他都不需要权限
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/manage").hasRole("admin")
+                .antMatchers("/manage/**").hasRole("admin")
                 .anyRequest().permitAll()
 
                 .and()
                 .formLogin().loginProcessingUrl("/login")
                 .permitAll()
                 .and()
-                .logout().permitAll()
+                .logout().logoutUrl("/logout")
+                .logoutSuccessHandler((req, resp, authentication) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(JSON.toJSONString(ServiceResult.error(
+                            new ServiceResult.DefaultMessage<>("00000000", "登出成功"))));
+                    out.flush();
+                    out.close();
+                })
+                .permitAll()
                 .and()
-                .csrf().disable();
+                .csrf().disable().exceptionHandling()
+                .authenticationEntryPoint((req, resp, authException) -> {
+                            resp.setContentType("application/json;charset=utf-8");
+                            PrintWriter out = resp.getWriter();
+                            out.write(JSON.toJSONString(ServiceResult.error(
+                                    new ServiceResult.DefaultMessage<>("10000005", "尚未登陆，请先登录"))));
+                            out.flush();
+                            out.close();
+                        }
+                );
 
         http.addFilter(customAuthenticationFilter());
 
@@ -76,19 +98,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 out.close();
             }
         });
+
+        /*
+        从 10000000 开始是错误码
+         */
         filter.setAuthenticationFailureHandler(new AuthenticationFailureHandler() {
             @Override
             public void onAuthenticationFailure(HttpServletRequest req, HttpServletResponse resp, AuthenticationException e) throws IOException, ServletException {
                 resp.setContentType("application/json;charset=utf-8");
                 PrintWriter out = resp.getWriter();
-                out.write(JSON.toJSONString(ServiceResult.error(
-                        new ServiceResult.DefaultMessage<>("00000003", "账号或者密码错误")
-                )));
+                if (e instanceof LockedException) {
+                    out.write(JSON.toJSONString(ServiceResult.error(
+                            new ServiceResult.DefaultMessage<>("10000000", "账户被锁定，请联系管理员!"))));
+                } else if (e instanceof CredentialsExpiredException) {
+                    out.write(JSON.toJSONString(ServiceResult.error(
+                            new ServiceResult.DefaultMessage<>("10000001", "密码过期，请联系管理员!"))));
+                } else if (e instanceof AccountExpiredException) {
+                    out.write(JSON.toJSONString(ServiceResult.error(
+                            new ServiceResult.DefaultMessage<>("10000002", "账户过期，请联系管理员!"))));
+                } else if (e instanceof DisabledException) {
+                    out.write(JSON.toJSONString(ServiceResult.error(
+                            new ServiceResult.DefaultMessage<>("10000003", "账户被禁用，请联系管理员!"))));
+                } else if (e instanceof BadCredentialsException) {
+                    out.write(JSON.toJSONString(ServiceResult.error(
+                            new ServiceResult.DefaultMessage<>("10000004", "用户名或者密码输入错误，请重新输入!"))));
+                }
                 out.flush();
                 out.close();
             }
-
         });
+        //.logoutSuccessHandler((req, resp, authentication) -> {
         filter.setAuthenticationManager(authenticationManagerBean());
         return filter;
     }
